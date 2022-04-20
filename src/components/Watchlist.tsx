2c837c158d0eedd4befff6eb4987f3c8c5d55f0c
Mary Mazi <mazi.mary.o@gmail.com>
2022-04-20 16:41:43 -0500

    feat (watchlist): add trackr watchlist input field

--- src/components/Watchlist.tsx
+++ src/components/Watchlist.tsx
@@ -1,7 +1,19 @@
 import React, { ReactElement } from 'react';
+import { Container, Box, Heading, Input, Button, HStack } from '@chakra-ui/react'
 
 const Watchlist = (): ReactElement => (
-  <h2>Stock Watch List</h2>
+  <>
+    <Box p={6} shadow='md' borderWidth='1px'>
+      <Heading fontSize='xl'>Trackr Watchlist</Heading>
+    </Box>
+
+    <Container>
+      <HStack spacing='12px' mt='24px'>
+        <Input placeholder='Add Ticker' />
+        <Button colorScheme='blue'>Submit</Button>
+      </HStack>
+    </Container>
+  </>
 );
 
 export default Watchlist;
