ea08ffc2b7db852b1fd87a863d1ed19b06335662
Mary Mazi <mazi.mary.o@gmail.com>
2022-04-21 05:32:42 -0500

    chore (server):
    add express server to serve app on Heroku

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const PORT = process.env.PORT || 9500;
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(PORT);
